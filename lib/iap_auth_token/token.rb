2a5e408c07f001ec520bd1a9c77007f5c59769ef
Sankalp Singh <singh.sankalp1@gmail.com>
2019-03-21 10:09:32 +0530

    add spec for token class

--- lib/iap_auth_token/token.rb
+++ lib/iap_auth_token/token.rb
@@ -5,7 +5,17 @@ module IapAuthToken
 
 		def self.generate_bearer_token( assertion )
 			uri = URI(TokenURI)
-			res = Net::HTTP.post_form(uri, 'grant_type' => JWTBearerType, 'assertion' => assertion)
+			begin
+				res = Net::HTTP.post_form(uri, 'grant_type' => JWTBearerType, 'assertion' => assertion)
+			rescue => e
+				raise e
+			end
+			if res.code.to_i != 200
+				error_description = JSON.parse(res.body)["error_description"]
+				error = JSON.parse(res.body)["error"]
+			 	raise("Request failed with error: #{error} and description: #{error_description}")
+			end
+
 			response_jwt = JSON.parse(res.body)["id_token"]
 			return response_jwt
 		end
